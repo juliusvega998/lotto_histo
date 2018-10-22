504ceb961a054486bfbb95530a435f1eaf1d888b
Julius Vega <juliusvega998@gmail.com>
2018-10-22 21:16:14 +0800

    Completed main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 from bs4 import BeautifulSoup
 import httplib2
 import datetime
-import json
-import boto3
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch
+FILE_PATH='D:/Documents/codes/python/lotto_histo/lotto.txt'
 
 def getHTML(url):
     http = httplib2.Http()
@@ -14,17 +11,45 @@ def getHTML(url):
     else:
         raise Exception('URL returned the following code: ' + response.status)
 
-def getNumber():
+def getNumbers():
     URL = 'https://www.philippinepcsolotto.com/history/6-58-lotto-result-history'
     soup = BeautifulSoup(getHTML(URL), 'html.parser')
-    #main_div = soup.select("div", {"class": "post-content box mark-links entry-content"})
-    result = soup.select('div.post-content.box.mark-links.entry-content > ul > li')[0].getText()
-    return result.split('-', 1)[1].strip()
+    
+    results = soup.select('div.post-content.box.mark-links.entry-content > ul > li')
+    numbers= []
+    for result in results:
+        numbers.append(result.getText().split('-', 1)[1].strip())
+    return numbers
+
+def getLotto():
+    lotto = open(FILE_PATH, 'r')
+    lotto_numbers = lotto.read().split('\n')
+    lotto.close()
+
+    return lotto_numbers
+
+def appendLotto(numbers):
+    lotto = open(FILE_PATH, 'a')
+    for number in numbers:
+        lotto.write('\n' + number)
+
+def cmpNumbers():
+    numbers = getNumbers()
+    lottos = getLotto()
+
+    for lotto in lottos:
+        for number in numbers:
+            if lotto == number:
+                numbers.remove(number)
+                break
+
+    if len(numbers) != 0:
+        appendLotto(numbers)
 
 def main():
     now = datetime.datetime.now()
     day = int(now.strftime('%w'))
     if day == 1 or day == 2 or day == 6:
-        print(getNumber())
+        cmpNumbers()
 
 main()
